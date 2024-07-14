6493d5ddffb7ce4467cde9d87bcc78f4be9762a6
Senshiacy <129632321+nathaliayukimi@users.noreply.github.com>
2024-07-14 09:47:47 -0300

    Add files via upload

---
+++ aval.js
@@ -0,0 +1,140 @@
+let symbolGraph = new Map();
+let ranking = new Map();
+let bool = false;
+
+async function prepare() {
+    try {
+        symbolGraph = new Map();
+        ranking = new Map();
+        
+        let response = await fetch('Empresas.json');
+        
+        if (!response.ok) {
+            bool = false;
+            throw new Error('HTTP error');
+        }
+
+        bool = true;
+        
+        let data = await response.json();
+
+        // Ordenar os dados pelo valor de `value.avaliacao`
+        data.sort((a, b) => b.avaliacao.localeCompare(a.avaliacao));
+
+        data.forEach(value => {
+            symbolGraph.set(value.empresa.toUpperCase().trim().replace(" ", ""), [value.porcentagem, value.img, value.tempo, value.progressao, value.ramo, value.avaliacao, value.site, value.empresa, value.salario]);
+        });
+        
+        data.forEach(value => {
+            ranking.set(value.avaliacao.toUpperCase().trim().replace(" ", ""), [value.porcentagem, value.img, value.tempo, value.progressao, value.ramo, value.empresa, value.site, value.salario]);
+        });
+
+        const atual = document.querySelector('.review-cards');
+        atual.innerHTML = ''; 
+
+        ranking.forEach((type, empresa) => {
+            const reviewCard = `
+                <div class="review-card">
+                    <img src="${type[1]}" alt="${empresa}">
+                    <h3>${type[5]}</h3>
+                    <a href="${type[6]}" class="site">Site</a>
+                    <p>Porcentagem de mulheres na empresa: ${type[0]}</p>
+                    <p>Tempo médio de duração dos cargos: ${type[2]}</p>
+                    <p>Expectativa de progressão de cargo: ${type[3]}</p>
+                    <p>Média Salarial: ${type[7]}</p>
+                    <p>Ramo: ${type[4]}</p>
+                    <p>Avaliação: ${empresa}</p>
+                </div>`;
+            atual.innerHTML += reviewCard;
+        });
+    } catch (error) {
+        bool = false;
+        console.log('HTTP server not available, keeping static content');
+        // Mantém o conteúdo HTML estático
+    }
+}
+
+function find(NomeEmpresa) {
+    if(!bool){
+        return;
+    }
+    let type = symbolGraph.get(NomeEmpresa);
+
+    if (!type) {
+        alert('Empresa não encontrada :(');
+        return;
+    }
+
+    const atual = document.querySelector('.review-cards');
+    atual.innerHTML = ''; 
+
+    atual.innerHTML = `
+        <div class="review-card">
+            <img src="${type[1]}" alt="${NomeEmpresa}">
+            <h3>${type[7]}</h3>
+            <a href="${type[6]}" class="site">Site</a>
+            <p>Porcentagem de mulheres na empresa: ${type[0]}</p>
+            <p>Tempo médio de duração dos cargos: ${type[2]}</p>
+            <p>Expectativa de progressão de cargo: ${type[3]}</p>
+            <p>Média Salarial: ${type[8]}</p>
+            <p>Ramo: ${type[4]}</p>
+            <p>Avaliação: ${type[5]}</p>
+        </div>`; 
+}
+
+function checkEnter(event) {
+    if (event.key === "Enter" && bool) {
+        let NomeEmpresa = document.querySelector(".busca").value.toUpperCase().trim().replace(" ", "");
+        find(NomeEmpresa);
+    }
+}
+
+function applyFilters() {
+    if(!bool){
+        return;
+    }
+    const selectedCategory = document.getElementById('category').value;
+    const atual = document.querySelector('.review-cards');
+    atual.innerHTML = ''; 
+
+    ranking.forEach((type, empresa) => {
+        if ((selectedCategory === "all" || type[4] === selectedCategory)) {
+            const reviewCard = `
+                <div class="review-card">
+                    <img src="${type[1]}" alt="${empresa}">
+                    <h3>${type[5]}</h3>
+                    <a href="${type[6]}" class="site">Site</a>
+                    <p>Porcentagem de mulheres na empresa: ${type[0]}</p>
+                    <p>Tempo médio de duração dos cargos: ${type[2]}</p>
+                    <p>Expectativa de progressão de cargo: ${type[3]}</p>
+                    <p>Média Salarial: ${type[7]}</p>
+                    <p>Ramo: ${type[4]}</p>
+                    <p>Avaliação: ${empresa}</p>
+                </div>`;
+            atual.innerHTML += reviewCard;
+        }
+    });
+}
+
+window.onload = function() {
+    prepare();
+    var input = document.querySelector('.search-bar .busca');
+    input.addEventListener('keydown', checkEnter);
+    input.value = "";  // Adicione esta linha para limpar o input
+    var value = "all";
+    document.getElementById('category').value = value;
+}
+
+document.querySelector(".submit").addEventListener("click", function(event){
+    event.preventDefault();
+    let NomeEmpresa = document.querySelector(".busca").value.toUpperCase().trim().replace(" ", "");
+    find(NomeEmpresa);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const selectElements = document.querySelectorAll('#category, #rating');
+
+    selectElements.forEach(selectElement => {
+        selectElement.addEventListener('change', applyFilters);
+    });
+});
